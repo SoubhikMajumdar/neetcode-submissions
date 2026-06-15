a16c5070be90f0ba362e67ea4cc6503924d3b30e
Soubhik Majumdar <55124230+SoubhikMajumdar@users.noreply.github.com>
2026-06-15 17:00:36 -0400

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        seen = {}
+        for i, num in enumerate(nums):
+            compliment = target - num
+            if compliment in seen:
+                return [seen[compliment],i]
+            seen[num] = i
