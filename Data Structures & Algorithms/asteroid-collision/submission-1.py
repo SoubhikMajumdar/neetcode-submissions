e9f4c11bda1f9b955fb1f845af4325c8d5557a6f
Soubhik Majumdar <55124230+SoubhikMajumdar@users.noreply.github.com>
2026-07-20 14:51:03 -0400

    Add: asteroid-collision - submission-1

---
+++ Data Structures & Algorithms/asteroid-collision/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for a in asteroids:
+            while stack and stack[-1] > 0 and a < 0: #while stack is not emoty and top of stack and incoming asteriod are +-
+                sign = a + stack[-1] #sign < 0 means stack element on top will be reomve and new asteriod takes its place
+                if sign < 0:
+                    stack.pop()
+                elif sign == 0:
+                    stack.pop()
+                    a = 0 #if sign is +- and equal  magn then incoming asteriod will not survive
+                else:
+                    a = 0
+            if a:
+                stack.append(a)
+        return stack
