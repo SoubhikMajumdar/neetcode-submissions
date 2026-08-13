45a210b6fd9e97df1c545bb573b26edc27ba83e3
Soubhik Majumdar <55124230+SoubhikMajumdar@users.noreply.github.com>
2026-08-13 16:37:14 -0400

    Add: linked-list-cycle-detection - submission-7

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-7.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+'''
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        hashset = set()
+        curr = head
+        if not curr: # if it doesnt exist then obvs false
+            return False
+        while curr:
+            if curr.next == None: #if we reach None, then it's not cyclic and ave reached end of LL
+                return False
+            else:
+                if curr in hashset: #if the listnode object already in hashset then we know it's cyclic
+                    return True
+                else:
+                    hashset.add(curr) # if havent seen in hash we can now add it to hashset
+            curr = curr.next '''
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+            
+        return False
+
+
+
