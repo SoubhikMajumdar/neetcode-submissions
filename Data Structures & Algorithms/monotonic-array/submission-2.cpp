41e416d27ece1647bae13e47582f049e801ae85b
Soubhik Majumdar <55124230+SoubhikMajumdar@users.noreply.github.com>
2026-08-19 15:41:31 -0400

    Add: monotonic-array - submission-2

---
+++ Data Structures & Algorithms/monotonic-array/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        bool decreasing = true;
+        bool increasing = true;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] > nums[i-1]){
+                decreasing = false;
+            }
+            if (nums[i] < nums[i-1]){
+                increasing = false;
+            }
+        }
+        return increasing or decreasing;
+    }
+};
