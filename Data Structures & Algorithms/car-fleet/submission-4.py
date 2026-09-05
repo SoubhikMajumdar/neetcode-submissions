7890d1374d0deb353235425e45ddbf9d1b44594f
Soubhik Majumdar <55124230+SoubhikMajumdar@users.noreply.github.com>
2026-09-05 14:42:21 -0400

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def carFleet(self, target, position, speed):
+        cars = sorted(zip(position, speed), reverse=True)
+
+        fleets = 0
+        slowest_time = 0
+
+        for p, s in cars:
+            time = (target - p) / s
+
+            if time > slowest_time:
+                fleets += 1
+                slowest_time = time
+
+        return fleets
